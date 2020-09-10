172ae88902f1828efa6189b567cc47c8c163c107
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-09-09 23:00:14 -0400

    Initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float minSpeed = 8f;
+    [SerializeField] float maxSpeed = 12f;
+    [SerializeField] float speed = 1f;
+
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnDelay = 0.3f;
+    [SerializeField] GameObject car;
+    public Transform[] spawnPoints;
+    float nextTimeToSpawn = 0f;
+
+    void Update()
+    {
+        if (nextTimeToSpawn <= Time.time)
+        {
+            SpawnCar();
+            nextTimeToSpawn = Time.time + spawnDelay;
+        }
+    }
+
+    void SpawnCar()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[randomIndex];
+
+        Instantiate(car, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
